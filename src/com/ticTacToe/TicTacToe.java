3e432352294d54048400af461a107ebfcd29474b
Ananthu R V <ananthurv666@hotmail.com>
2015-03-23 09:11:50 +0530

    created add marker method to add only 'x' mark for now added winning for rows and columns

--- src/com/ticTacToe/TicTacToe.java
+++ src/com/ticTacToe/TicTacToe.java
@@ -1,4 +1,56 @@
 package com.ticTacToe;
 
 public class TicTacToe {
+    private char[][] board;
+
+    public TicTacToe() {
+        this.board = new char[3][3];
+        board[0][0] = 1;
+        board[0][1] = 2;
+        board[0][2] = 3;
+        board[1][0] = 4;
+        board[1][1] = 5;
+        board[1][2] = 6;
+        board[2][0] = 7;
+        board[2][1] = 8;
+        board[2][2] = 9;
+    }
+
+    public void addMarker(int position) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == position)
+                    board[i][j] = 'x';
+            }
+        }
+    }
+
+    public char[][] getBoard() {
+        return this.board;
+    }
+
+    public boolean winning() {
+        char current = ' ';
+        //row
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (Character.isDigit(board[i][j])) break;
+                if (j == 0)
+                    current = board[i][j];
+                else if (current != board[i][j]) break;
+                if (j == 2) return true;
+            }
+        }
+        //column
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (Character.isDigit(board[i][j])) break;
+                if (j == 0)
+                    current = board[i][j];
+                else if (current != board[j][i]) break;
+                if (j == 2) return true;
+            }
+        }
+        return false;
+    }
 }
--- test/com/ticTacToe/TicTacToeTest.java
+++ test/com/ticTacToe/TicTacToeTest.java
@@ -2,9 +2,30 @@ package com.ticTacToe;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class TicTacToeTest {
     @Test
-    public void testWinningReturnsTrueIfPlayerWins(){
+    public void testAddMarkerAddsTheMarkOnTheBoard(){
         TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.addMarker(1);
+        assertEquals('x',ticTacToe.getBoard()[0][0]);
+    }
+    @Test
+    public void testWinningReturnsTrueIfPlayerAddsMarkerOnSameRow(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.addMarker(1);
+        ticTacToe.addMarker(2);
+        ticTacToe.addMarker(3);
+        assertTrue(ticTacToe.winning());
+    }
+    @Test
+    public void testWinningReturnsTrueIfPlayerAddsMarkerOnSameColumn(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.addMarker(1);
+        ticTacToe.addMarker(4);
+        ticTacToe.addMarker(7);
+        assertTrue(ticTacToe.winning());
     }
 }
