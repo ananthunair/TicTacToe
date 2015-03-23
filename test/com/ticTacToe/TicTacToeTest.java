adc0460f033c0f5b3bffe2c72fc5496a2c67577e
Kittu Jain <cskratijain@gmail.com>
2015-03-23 10:20:04 +0530

    written test for false condition of different columns winning condition rechecked

--- test/com/ticTacToe/TicTacToeTest.java
+++ test/com/ticTacToe/TicTacToeTest.java
@@ -1,8 +1,8 @@
 package com.ticTacToe;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TicTacToeTest {
@@ -12,6 +12,7 @@ public class TicTacToeTest {
         ticTacToe.addMarker(1);
         assertEquals('x',ticTacToe.getBoard()[0][0]);
     }
+
     @Test
     public void testWinningReturnsTrueIfPlayerAddsMarkerOnSameRow(){
         TicTacToe ticTacToe = new TicTacToe();
@@ -20,6 +21,7 @@ public class TicTacToeTest {
         ticTacToe.addMarker(3);
         assertTrue(ticTacToe.winning());
     }
+
     @Test
     public void testWinningReturnsTrueIfPlayerAddsMarkerOnSameColumn(){
         TicTacToe ticTacToe = new TicTacToe();
@@ -27,5 +29,14 @@ public class TicTacToeTest {
         ticTacToe.addMarker(4);
         ticTacToe.addMarker(7);
         assertTrue(ticTacToe.winning());
+    }
+
+    @Test
+    public void testWinningReturnsFalseIfPlayerAddsMarkerOnDifferentColumn(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.addMarker(2);
+        ticTacToe.addMarker(4);
+        ticTacToe.addMarker(7);
+        assertFalse(ticTacToe.winning());
     }
 }
